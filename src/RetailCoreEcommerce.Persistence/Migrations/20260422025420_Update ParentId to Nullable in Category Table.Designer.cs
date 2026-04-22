b56dab2f63cf105d2d1b84ae339e157ade7013e7
khanght <khanghuynhtan.work@gmail.com>
2026-04-22 09:55:14 +0700

    [Add]: Add migration

--- src/RetailCoreEcommerce.Persistence/Migrations/20260422025420_Update ParentId to Nullable in Category Table.Designer.cs
+++ src/RetailCoreEcommerce.Persistence/Migrations/20260422025420_Update ParentId to Nullable in Category Table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RetailCoreEcommerce.Persistence.Data;
 
@@ -11,9 +12,11 @@ using RetailCoreEcommerce.Persistence.Data;
 namespace RetailCoreEcommerce.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260422025420_Update ParentId to Nullable in Category Table")]
+    partial class UpdateParentIdtoNullableinCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
