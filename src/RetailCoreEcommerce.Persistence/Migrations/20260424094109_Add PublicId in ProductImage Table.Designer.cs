60425f2e9749e93feac68cc0672df6ac10d7a9ed
khanght <khanghuynhtan.work@gmail.com>
2026-04-24 16:44:50 +0700

    [Add]: Add publicId field migration for product image table

--- src/RetailCoreEcommerce.Persistence/Migrations/20260424094109_Add PublicId in ProductImage Table.Designer.cs
+++ src/RetailCoreEcommerce.Persistence/Migrations/20260424094109_Add PublicId in ProductImage Table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RetailCoreEcommerce.Persistence.Data;
 
@@ -11,9 +12,11 @@ using RetailCoreEcommerce.Persistence.Data;
 namespace RetailCoreEcommerce.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260424094109_Add PublicId in ProductImage Table")]
+    partial class AddPublicIdinProductImageTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/RetailCoreEcommerce.Persistence/Migrations/20260424094109_Add PublicId in ProductImage Table.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RetailCoreEcommerce.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPublicIdinProductImageTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PublicId",
+                table: "ProductImage",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "ProductImage");
+        }
+    }
+}
